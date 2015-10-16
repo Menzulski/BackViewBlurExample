ff01a6f4f56ce85fd5e1a74f39aff7a2d6acff05
Stanislav Menzulski <menzulski@macbook-pro-stanislav.local>
2015-10-16 11:58:36 +0300

    Initial Commit

---
+++ BackViewBlurExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackViewBlurExample
+//
+//  Created by Stanislav Menzulski on 16.10.15.
+//  Copyright © 2015 Stanislav Menzulski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackViewBlurExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackViewBlurExample
+//
+//  Created by Stanislav Menzulski on 16.10.15.
+//  Copyright © 2015 Stanislav Menzulski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
